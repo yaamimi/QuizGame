c57791a247999ebc4a79077acc454afa82da2500
YoussefAMIMI <youssefamine.amimi@gmail.com>
2023-01-12 21:53:15 +0100

    Manage EditText Keyboard behavior

--- app/src/main/java/com/example/quizapplication/ui/WelcomeFragment.kt
+++ app/src/main/java/com/example/quizapplication/ui/WelcomeFragment.kt
@@ -2,16 +2,19 @@ package com.example.quizapplication.ui
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.quizapplication.R
 import com.example.quizapplication.Utils
 import com.example.quizapplication.databinding.FragmentWelcomeBinding
+
 
 class WelcomeFragment : Fragment() {
     lateinit var navController: NavController
@@ -21,7 +24,7 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentWelcomeBinding.inflate(inflater,container,false)
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         (requireActivity() as MainActivity).title = getString(R.string.welcome_title)
         return binding.root
     }
@@ -29,11 +32,24 @@ class WelcomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
+        binding.name.setOnFocusChangeListener { v, b ->
+            if (!b) {
+                val imm: InputMethodManager? =
+                    requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+                imm?.hideSoftInputFromWindow(v.windowToken, 0)
+            }
+        }
+        binding.name.setOnEditorActionListener { textView, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE)
+                textView.clearFocus()
+            false
+        }
         binding.startButton.setOnClickListener {
             if (binding.name.text.toString().isEmpty())
-                Toast.makeText(context,getString(R.string.set_name_error),Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, getString(R.string.set_name_error), Toast.LENGTH_SHORT)
+                    .show()
             else {
-                Utils.saveAtSharedPrefs(requireActivity(),binding.name.text.toString())
+                Utils.saveAtSharedPrefs(requireActivity(), binding.name.text.toString())
                 navController.navigate(R.id.action_welcomeFragment_to_chooseThemeFragment)
             }
         }
